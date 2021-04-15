f75a577617231a89bd010d0162fdf53c4d9d0c2c
masanor57 <miyajim2020@gmail.com>
2021-04-15 11:31:03 +0000

    update

---
+++ app/controllers/report_comments_controller.rb
@@ -0,0 +1,7 @@
+class ReportCommentsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+ root 'homes#top'
+ devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'homes#top'
+ resources :users, only: [:index, :show, :edit, :update]
+ resources :reports, only: [:index, :show, :edit, :create, :destroy, :update] 
 
 
 
