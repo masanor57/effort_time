416a285a3f30d2ffd922d6915ada027393c3a2b5
masanor57 <miyajim2020@gmail.com>
2021-04-17 09:19:39 +0000

    更新

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,21 +1,36 @@
 class ReportsController < ApplicationController
+
   def index
+    @user = current_user
+    @reports = Report.all
+    @report = Report.new
   end
 
   def show
+    @report = Report.find(params[:id])
   end
 
  def create
     @report = Report.new(report_params)
     @report.user_id = current_user.id
- end
-
-  def edit
+  if @report.save
+      redirect_to reports_path(@report), notice: "You have created book successfully."
+  else
+      @reports = Report.all
+      render 'index'
   end
+ end
 
   def update
   end
 
   def destroy
   end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
  root 'homes#top'
  devise_for :users
+ get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  resources :users, only: [:index, :show, :edit, :update]
- resources :reports, only: [:index, :show, :edit, :create, :destroy, :update] 
+ resources :reports, only: [:index, :show, :edit, :create, :destroy, :update]
 
 
 
