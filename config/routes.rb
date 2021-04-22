87d2cbdd869f2c197f5ce9cd48aee42490789f3c
masanor57 <miyajim2020@gmail.com>
2021-04-22 14:33:48 +0000

    follower unfinish

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
  devise_for :users
  get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- resources :users, only: [:index, :show, :edit, :update]
+ resources :users, only: [:index, :show, :edit, :update] do
+  resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings'
+  get 'followers' => 'relationships#followers'
+  end
+
  resources :reports, only: [:index, :show, :edit, :create, :destroy, :update] do
   resource :favorites, only: [:create, :destroy]
   resources :report_comments, only: [:create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_091316) do
+ActiveRecord::Schema.define(version: 2021_04_22_135624) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "report_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "following_id"], name: "index_relationships_on_follower_id_and_following_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["following_id"], name: "index_relationships_on_following_id"
   end
 
   create_table "report_comments", force: :cascade do |t|
