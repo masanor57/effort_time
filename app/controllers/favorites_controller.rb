599365f0f7b4b00aa9053d498bbd55d036a57467
masanor57 <miyajim2020@gmail.com>
2021-05-25 14:13:29 +0000

    ajax 1

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,11 @@ class FavoritesController < ApplicationController
     report = Report.find(params[:report_id])
     favorite = current_user.favorites.new(report_id: report.id)
     favorite.save
-    redirect_to reports_path(report)
   end
 
   def destroy
     report = Report.find(params[:report_id])
     favorite = current_user.favorites.find_by(report_id: report.id)
     favorite.destroy
-    redirect_to reports_path(report)
   end
 end
