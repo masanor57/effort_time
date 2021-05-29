e0e90e6d18a6d39f1f67e48abfb64f8b521c8f66
masanor57 <miyajim2020@gmail.com>
2021-05-29 11:25:23 +0000

    ajax ３

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,9 +6,9 @@ class SearchController < ApplicationController
      @content = params[:content]
      @method = params[:method]
  if  @model == 'user'
-	 @records = User.search_for(@content, @method)
+	   @records = User.search_for(@content, @method)
  else
-	 @records = Report.search_for(@content, @method)
+	   @records = Report.search_for(@content, @method)
  end
  end
 
