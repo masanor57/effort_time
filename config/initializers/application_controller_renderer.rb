4f5817b1a297a8f054b4c015ee0cb0f86baaf805
masanor57 <miyajim2020@gmail.com>
2021-06-04 14:18:24 +0000

    profileimage1

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fe66b199b41e9e10d7090d8c353f015c58a4b9ca5fbbe90e62da8322822f4166a800f52a76a816973e76d1436a13635a4319f935f1fe94156ae173ff91a9fb87'
